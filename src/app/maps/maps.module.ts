c6c19bb58ca0eab460494b2d569262d49b7d0391
Candela Landi <landicandela01@gmail.com>
2024-04-17 07:15:44 -0300

    add components

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MapsRoutingModule } from './maps-routing.module';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent
+  ],
   imports: [
     CommonModule,
     MapsRoutingModule
